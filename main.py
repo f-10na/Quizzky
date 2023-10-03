17050843cb21415d0f8db2d1433ed7b293edb4a6
f-10na <fionakissi5@gmail.com>
2023-10-03 22:17:44 +0100

    Spell quizzky
    code now gets user to spell quizzky and they lose when they run out of letters

--- main.py
+++ main.py
@@ -1,7 +1,72 @@
-print("Welcome to Quizzky:)")
-count = 0
-chances = int(input("How many chances do you want?"))
-while chances < count:
-  print("questions")
+import random
 
-quit()        
+print("Welcome to Quizzky:)")
+
+count = 0
+quizzkydict = {
+  1: "Q",
+  2: "QU",
+  3: "QUI",
+  4: "QUIZ",
+  5: "QUIZZ",
+  6: "QUIZZK",
+  7: "QUIZZKY"
+}
+
+answers = []
+
+def get_capitals():
+    try:
+        with open("questions.txt", "r") as f:
+            for line in f:
+                a = line.strip()  # Strip off the \n
+                answer = a.split(",")  # Split the string
+                answers.append(answer)
+        return answers
+    except FileNotFoundError:
+        print("Error: The 'questions.txt' file was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+get_capitals()
+
+countries = []
+
+def get_countries():
+    try:
+        with open("questions.txt", "r") as f:
+            for line in f:
+                country, _ = line.strip().split(",")  # Split the string into country and capital
+                countries.append(country)  # Append only the country to the answers list
+        return countries
+    except FileNotFoundError:
+        print("Error: The 'questions.txt' file was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+get_countries()
+
+def find_country(country):
+    for answer in answers:
+        if answer[0] == country:
+            return answer[1]
+    
+
+while count >= 0:
+    question_file = random.choice(countries)
+    print(question_file)
+    question = question_file
+    answer = input("type your answer: ").lower()
+    
+    # Check if the answer is correct
+    if answer == find_country(question):
+        count += 1
+        print(quizzkydict.get(count))
+        print("Correct! Spell the next part.")
+        if count == len(quizzkydict):
+           print("Congratulations! You have spelled 'Quizzky' correctly!") 
+    #decrements the count when the answer is wrong
+    else:
+        print("Incorrect! Try again.")
+        print(quizzkydict.get(count))
+        count -= 1 
